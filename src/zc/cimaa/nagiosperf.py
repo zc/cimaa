9ff863677cdc0df194c5db07973bf3e97a7839b3
Jim Fulton <jim@zope.com>
2014-12-14 09:34:23 -0500

    values must be numbers

--- src/zc/cimaa/nagiosperf.py
+++ src/zc/cimaa/nagiosperf.py
@@ -10,7 +10,7 @@ r"""Parser for nagios performance data
     >>> ppp(
     ... "DISK OK - free space: / 3326 MB (56%); | /=2643MB;5948;5958;0;5968")
     ('DISK OK - free space: / 3326 MB (56%); \n',
-     [{'name': '/', 'units': 'MB', 'value': '2643'}])
+     [{'name': '/', 'units': 'MB', 'value': 2643.0}])
 
     >>> ppp(
     ... '''DISK OK - free space: / 3326 MB (56%); | /=2643MB;5948;5958;0;5968
@@ -23,19 +23,19 @@ r"""Parser for nagios performance data
     ('DISK OK - free space: / 3326 MB (56%);
        \n/ 15272 MB (77%);\n/boot 68 MB (69%);\n/home 69357 MB
        (27%);\n/var/log 819 MB (84%); ',
-     [{'name': '/', 'units': 'MB', 'value': '2643'},
-      {'name': '/boot', 'units': 'MB', 'value': '68'},
-      {'name': '/home', 'units': 'MB', 'value': '69357'},
-      {'name': '/var/log', 'units': 'MB', 'value': '818'}])
+     [{'name': '/', 'units': 'MB', 'value': 2643.0},
+      {'name': '/boot', 'units': 'MB', 'value': 68.0},
+      {'name': '/home', 'units': 'MB', 'value': 69357.0},
+      {'name': '/var/log', 'units': 'MB', 'value': 818.0}])
 
     >>> ppp("PING ok - Packet loss = 0%, RTA = 0.80 ms "
     ...              "| percent_packet_loss=0, rta=0.80")
     ('PING ok - Packet loss = 0%, RTA = 0.80 ms \n',
-     [{'name': 'percent_packet_loss', 'units': ',', 'value': '0'},
-      {'name': 'rta', 'units': '', 'value': '0.80'}])
+     [{'name': 'percent_packet_loss', 'units': ',', 'value': 0.0},
+      {'name': 'rta', 'units': '', 'value': 0.8}])
 
     >>> ppp("| 'ha ha ha'=3has")
-    ('\n', [{'name': "'ha ha ha'", 'units': 'has', 'value': '3'}])
+    ('\n', [{'name': "'ha ha ha'", 'units': 'has', 'value': 3.0}])
 
 See: https://nagios-plugins.org/doc/guidelines.html#AEN200
 """
@@ -60,6 +60,7 @@ def parse_output(text):
             (rest[1].replace('\n', '') if len(rest) > 1 else ''))
     return (
         first[0] + '\n' + rest[0],
-        [dict(name=m[0], value=m[1], units=m[3]) for m in perf_parse(perf)],
+        [dict(name=m[0], value=float(m[1]), units=m[3])
+         for m in perf_parse(perf)],
         )
 
