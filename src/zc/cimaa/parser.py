3588e4498103dba0da7bc6d2a3c644b7027c96cb
Fred Drake <fred@zope.com>
2014-12-15 09:45:19 -0500

    control ConfigParser.optionxform the same way we do for buildout

--- src/zc/cimaa/parser.py
+++ src/zc/cimaa/parser.py
@@ -4,12 +4,14 @@ import ConfigParser
 
 def parse_text(text):
     parser = ConfigParser.RawConfigParser()
+    parser.optionxform = str
     import StringIO
     parser.readfp(StringIO.StringIO(text))
     return parser_dict(parser)
 
 def parse_file(name):
     parser = ConfigParser.RawConfigParser()
+    parser.optionxform = str
     parser.read(name)
     return parser_dict(parser)
 
